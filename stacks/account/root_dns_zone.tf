68aaf278aa0983e654074acaa827eec680ed1f82
pauldoom <pauldoom@gmail.com>
2021-04-25 15:00:42 -0500

    Fix yer formats

--- stacks/account/root_dns_zone.tf
+++ stacks/account/root_dns_zone.tf
@@ -2,7 +2,7 @@ locals {
   # Gross!  See https://github.com/hashicorp/terraform/issues/22405#issuecomment-625619906
   static_dns_records = try(
     var.static_dns_record_file == "" ? tomap(false) : yamldecode(file(var.static_dns_record_file)),
-    [])
+  [])
 }
 module "root_dns_zone" {
   source         = "../../modules/root_dns_zone"
