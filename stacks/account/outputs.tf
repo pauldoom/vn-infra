f4731fd0956e9b33a811249582b2978b0e095174
pauldoom <pauldoom@gmail.com>
2021-08-02 23:42:28 -0500

    Output DNSSEC KSK information

--- stacks/account/outputs.tf
+++ stacks/account/outputs.tf
@@ -6,3 +6,7 @@ output "root_zone_id" {
 output "root_zone_nameservers" {
   value = module.root_dns_zone.root_zone_nameservers
 }
+
+output "root_zone_key_signing_key" {
+  value = module.root_dns_zone.root_zone_key_signing_key
+}
